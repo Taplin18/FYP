93912e0d8e3163c482fbc55b354586a7b4736a1a
Lauren Taplin <laurentap96@gmail.com>
2018-04-19 03:00:32 +0100

    updated graphs

--- compareStats.py
+++ compareStats.py
@@ -27,32 +27,83 @@ class CompareResults:
     def ffp(self):
         keys = sorted(list(self.sbf_sizes.keys()))
 
+        for y in keys[:20]:
+            test = self.sbf_sizes[y]
+            filter_stats = test.get_stats()
+            print('size {}: {}'.format(y, filter_stats['Filter False Positive Probability']))
+
+    def graph_fpp(self):
+        objects = sorted(list(self.sbf_sizes.keys()))
+
         performance = []
-        for y in keys[6:21]:
+        for y in objects[:20]:
             test = self.sbf_sizes[y]
             filter_stats = test.get_stats()
             performance.append(filter_stats['Filter False Positive Probability'])
 
-    def plot_ffp(self):
-        keys = sorted(list(self.sbf_sizes.keys()))
-        y_pos = np.arange(len(keys[6:21]))
-
-        performance = []
-        for y in keys[6:21]:
-            test = self.sbf_sizes[y]
-            filter_stats = test.get_stats()
-            performance.append(filter_stats['Filter False Positive Probability'])
-
-        plt.figure(num=None, figsize=(12, 8), dpi=80, facecolor='w', edgecolor='k')
-        plt.bar(y_pos, performance, align='center', alpha=1)
-        plt.xticks(y_pos, keys[6:21])
+        fig = plt.figure(num=None, figsize=(12, 8), dpi=80, facecolor='w', edgecolor='k')
+        plt.title("False Positive Probability")
         plt.ylabel('False Positive Probability')
         plt.xlabel('Spatial Bloom Filter Sizes')
-        plt.title('False Positive Probability')
+        plt.plot(objects[:20], performance, 'bo-')
+        plt.gca().invert_yaxis()
+        plt.show()
 
+    def hash_collision(self):
+        keys = sorted(list(self.sbf_sizes.keys()))
+
+        for y in keys[:25]:
+            test = self.sbf_sizes[y]
+            filter_stats = test.get_stats()
+            print('size {}: {}'.format(y, filter_stats['Number of Hash Collisions']))
+
+    def graph_hash_col(self):
+        objects = sorted(list(self.sbf_sizes.keys()))
+
+        performance = []
+        for y in objects[:25]:
+            test = self.sbf_sizes[y]
+            filter_stats = test.get_stats()
+            performance.append(filter_stats['Number of Hash Collisions'])
+
+        fig = plt.figure(num=None, figsize=(12, 8), dpi=80, facecolor='w', edgecolor='k')
+        plt.title('Hash Collisions')
+        plt.ylabel('Number of Hash Collisions')
+        plt.xlabel('Spatial Bloom Filter Sizes')
+        plt.plot(objects[:25], performance, 'bo-')
+        plt.gca().invert_yaxis()
+        plt.show()
+
+    def sparsity(self):
+        keys = sorted(list(self.sbf_sizes.keys()))
+
+        for y in keys[:30]:
+            test = self.sbf_sizes[y]
+            filter_stats = test.get_stats()
+            print('size {}: {}'.format(y, filter_stats['Filter Sparsity']))
+
+    def graph_sparsity(self):
+        objects = sorted(list(self.sbf_sizes.keys()))
+
+        performance = []
+        for y in objects[:30]:
+            test = self.sbf_sizes[y]
+            filter_stats = test.get_stats()
+            performance.append(filter_stats['Filter Sparsity'])
+
+        fig = plt.figure(num=None, figsize=(12, 8), dpi=80, facecolor='w', edgecolor='k')
+        plt.title('Sparsity')
+        plt.ylabel('Filter Sparsity')
+        plt.xlabel('Spatial Bloom Filter Sizes')
+        plt.plot(objects[:30], performance, 'bo-')
         plt.show()
 
 
 if __name__ == '__main__':
     stats = CompareResults()
     stats.ffp()
+    stats.graph_fpp()
+    stats.hash_collision()
+    stats.graph_hash_col()
+    stats.sparsity()
+    stats.graph_sparsity()
